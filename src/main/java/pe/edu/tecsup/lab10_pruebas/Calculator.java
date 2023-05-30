c0c9080f8f7c6acfa2221b3df1beb3d55b938e36
Luis Corrales <luis.corrales@tecsup.edu.pe>
2023-05-30 17:09:09 -0500

    first commit

---
+++ src/main/java/pe/edu/tecsup/lab10_pruebas/Calculator.java
@@ -0,0 +1,10 @@
+package pe.edu.tecsup.lab10_pruebas;
+
+public class Calculator {
+	public int sub(int i, int j) {
+		return i - j;
+	}
+	public int add(int i, int j) {
+		return i + j;
+	}
+}
---
+++ src/test/java/pe/edu/tecsup/lab10_pruebas/CalculatorTest.java
@@ -0,0 +1,42 @@
+package pe.edu.tecsup.lab10_pruebas;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.core.config.Configurator;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class CalculatorTest {
+	private static final Logger log =
+				LogManager.getLogger(CalculatorTest.class);
+	Calculator app = new Calculator();
+	
+	@BeforeAll
+	static void initAll() {
+		Configurator.setRootLevel(org.apache.logging.log4j.Level.INFO);
+		log.info("initAll()...!");
+	}
+	
+	@BeforeEach
+	void init() {
+		log.info("init()....!");
+	}
+	
+	@Test
+	public void testAdd() {
+		log.info("testAdd()");
+		int EXPECTED_VALUE=7;
+		assertEquals(EXPECTED_VALUE, app.add(4, 3));
+	}
+
+	@Test
+	public void testSub() {
+		log.info("testSub()");
+		int EXPECTED_VALUE=1;
+		assertEquals(EXPECTED_VALUE, app.sub(4, 3));
+	}
+	
+}
